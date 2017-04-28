51dd132f8b67caccf283e68076235aaaaab450fb
Robert <robert.der.herzog@gmail.com>
2017-04-28 18:34:05 +0200

    ficken

---
+++ Assets/CylinderSpawner.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CylinderSpawner : MonoBehaviour
+{
+    public GameObject LEDPrefab;
+    public float radiusTop = 0.03f;
+    public float radiusBottom = 0.75f;
+
+    public List<Vector3> positions;
+
+    void Start()
+    {
+        positions = new List<Vector3>();
+        for(int ii = 0; ii <10; ii ++)
+        { var trns = transform.position;
+
+            if (ii == 0)
+            {
+                var pos = new Vector3(trns.x + radiusTop, trns.y, trns.z);
+                positions.Add(pos);
+            }
+            else if (ii < 2)
+            {
+                var pos = new Vector3(trns.x + radiusTop + (0.075f * (ii)), trns.y - (0.055f * (ii)), trns.z);
+                positions.Add(pos);
+            }
+            else if (ii < 7)
+            {
+                var pos = new Vector3(trns.x + radiusTop + (0.075f * (ii)), trns.y - (0.065f * (ii)), trns.z);
+                positions.Add(pos);
+            }
+            else
+            {
+                var pos = new Vector3(trns.x + radiusTop + (0.075f * (ii)), trns.y - (0.06f * (ii)), trns.z);
+                positions.Add(pos);
+            }
+
+        }
+        for (int ii = 0; ii < 10; ii++)
+        {
+            const float xadd = 0.075f * 9f + 0.0325f;
+            const float ysub = 0.065f * 9.0f + 0.048f;
+            var trns = transform.position;
+            var pos = new Vector3(trns.x + radiusTop + xadd, trns.y-ysub-0.06f*ii, trns.z);
+                positions.Add(pos);
+            
+
+        }
+
+        int num = 0;
+        foreach(Vector3 pos in positions)
+        {
+            if (LEDPrefab != null)
+            {
+                var led = Instantiate(LEDPrefab, pos, transform.rotation);
+                led.name = num.ToString();
+                led.transform.SetParent(transform);
+                num++;
+            }
+        }
+    }
+
+}
---
+++ Assets/main.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class main : MonoBehaviour {
+    public GameObject LedStringPrefab;
+    // Use this for initialization
+    bool oneshot ;
+    public List<GameObject> Strings;
+	void Start () {
+        Strings = new List<GameObject>();
+		if (LedStringPrefab != null)
+        {
+            for(int ii =0; ii <25; ii++)
+            {
+                
+                var ledstring = Instantiate(LedStringPrefab);
+                Strings.Add(ledstring);
+                
+            }
+        }
+        oneshot = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(oneshot)
+        {
+            int ii = 0;
+            foreach (GameObject ledstring in Strings)
+            {
+
+                ledstring.transform.Rotate(0f, 14.4f * ii, 0f);
+                ii++;
+            }
+            oneshot = false;
+        }
+	}
+}
