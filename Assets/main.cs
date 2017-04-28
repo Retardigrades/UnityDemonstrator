144c782495cb75813c686ba7e11be856ec0d0951
Robert <robert.der.herzog@gmail.com>
2017-04-28 19:05:52 +0200

    added  pingpong list of LEDS.

--- Assets/main.cs
+++ Assets/main.cs
@@ -7,7 +7,9 @@ public class main : MonoBehaviour {
     // Use this for initialization
     bool oneshot ;
     public List<GameObject> Strings;
+    public List<GameObject> LEDS;
 	void Start () {
+        LEDS = new List<GameObject>();
         Strings = new List<GameObject>();
 		if (LedStringPrefab != null)
         {
@@ -20,19 +22,52 @@ public class main : MonoBehaviour {
             }
         }
         oneshot = true;
+
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(oneshot)
         {
-            int ii = 0;
+            int kk = 0;
             foreach (GameObject ledstring in Strings)
             {
 
-                ledstring.transform.Rotate(0f, 14.4f * ii, 0f);
-                ii++;
+                ledstring.transform.Rotate(0f, 14.4f * kk, 0f);
+                kk++;
             }
+
+
+            for (int ii = 0; ii < Strings.Count; ii++)
+            {
+                Debug.Log(ii);
+                Debug.Log("childs :" + Strings[ii].transform.childCount);
+
+                if (Strings[ii].transform.childCount > 0)
+                {
+                    if (ii % 2 == 0)
+                    {
+                        for (int jj = 0; jj < Strings[ii].transform.childCount; jj++)
+                        {
+                            var parent = Strings[ii].transform;
+
+                            LEDS.Add(parent.GetChild(jj).gameObject);
+                        }
+                    }
+                    else
+                    {
+                        for (int jj = 0; jj < Strings[ii].transform.childCount; jj++)
+                        {
+                            var parent = Strings[ii].transform;
+
+                            LEDS.Add(parent.GetChild((parent.childCount - 1) - jj).gameObject);
+                        }
+                    }
+                }
+            }
+
             oneshot = false;
         }
 	}
