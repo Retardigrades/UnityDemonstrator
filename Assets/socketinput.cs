968eb574b74c6ba162cf19b210374e21797b0f5b
Robert Herzog <robert.herzog@hypnolords.com>
2017-05-05 11:08:33 +0200

    include udp protocol in socketinput

--- Assets/socketinput.cs
+++ Assets/socketinput.cs
@@ -9,57 +9,19 @@ public class socketinput : MonoBehaviour {
     public static int Port;
     // n Array wär mir ja lieber aber erstmal single input wär ja auch schon was.
     public Color DataOut;
+    UdpClient client = new UdpClient();
+    IPEndPoint endpoint = new IPEndPoint(IPAddress.Any, Port);
 
     public static byte[] DataIn;
-
-    //from: https://msdn.microsoft.com/en-us/library/kb5kfec7(v=vs.110).aspx  << lets see if it does the trick
-    public static void Startclient()
-    {
-        byte[] bytes = new byte[12];
-
-        try
-        { IPAddress IPadress = IPAddress.Parse(IP);
-          IPEndPoint remoteEP = new IPEndPoint(IPadress, Port);
-          Socket sender = new Socket(AddressFamily.InterNetwork,
-          SocketType.Stream, ProtocolType.Tcp);
-            try
-            {
-                sender.Connect(remoteEP);
-                int received = sender.Receive(bytes);
-                DataIn = bytes;
-
-                sender.Shutdown(SocketShutdown.Both);
-                sender.Close();
-            }
-            catch (ArgumentNullException ane)
-            {
-                Debug.Log("ArgumentNullException : {0}" + ane.ToString());
-            }
-            catch (SocketException se)
-            {
-                Debug.Log("SocketException : {0}" + se.ToString());
-            }
-            catch (Exception e)
-            {
-               Debug.Log("Unexpected exception : {0}" + e.ToString());
-            }
-
-        }
-        catch (Exception e)
-        {
-           Debug.Log(e.ToString());
-        }
-
-
-    }
+  
 
     //da war doch irgendwas mit 12 byte oder so.. keine Ahnung.. 
     Color BytetoColor(byte[] bytes)
     {
         Color col = new Color();
         col.r = BitConverter.ToSingle(bytes, 0);
-        col.g = BitConverter.ToSingle(bytes, 4);
-        col.b = BitConverter.ToSingle(bytes, 8);
+        col.g = BitConverter.ToSingle(bytes, 8);
+        col.b = BitConverter.ToSingle(bytes, 16);
         col.a = 1.0f;  // alpha is nich. 
         return col;
 
@@ -67,14 +29,23 @@ public class socketinput : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Startclient();
+        
         
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+    DataIn = client.Receive(ref endpoint);
 		if(DataIn.Length > 11)
         {
+            for(int i = 0; i < DataIn.Length; i = i+3) {
+        float r = DataIn[i] / 255.0f;
+        float g = DataIn[i + 1];
+        float b = DataIn[i + 2];
+
+        
+      }
             DataOut = BytetoColor(DataIn);
         }
 	}
